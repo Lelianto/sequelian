d8bd2cc1f5e29907f45f9e9749f70440e8e1f8a4
Lelianto Eko Pradana <lelianto.pradana@rupiahcepat.co.id>
2021-08-26 17:18:52 +0700

    add page not found and redirect in logo to home page

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="w-full shadow fixed left-0 right-0 top-0 z-50 bg-white">
     <div class="lg:container mx-4 flex gap-2 py-2">
-      <img id="main-image" class="h-8 w-8" src="@/assets/images/sqlLogo.png" alt="">
-      <pre><code id="main-title" class="text-lg">Sequelian</code></pre>
+      <NuxtLink to="/" class="text-black">
+        <img id="main-image" class="h-8 w-8" src="@/assets/images/sqlLogo.png" alt="">
+      </NuxtLink>
+      <NuxtLink to="/" class="text-black">
+        <pre><code id="main-title" class="text-lg">Sequelian</code></pre>
+      </NuxtLink>
     </div>
   </div>
 </template>
---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="lg:container mx-4">
+    <div class="pt-12">
+      <div class="text-3xl mb-5 font-bold">
+        Page Not Found
+      </div>
+      <NuxtLink to="/" class="text-xl cursor-pointer">
+        Go to <span class="text-blue-700 font-bold cursor-pointer">Home Page</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error'
+}
+</script>
+
+<style scoped>
+</style>
